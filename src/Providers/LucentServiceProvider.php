a46f95b0e64e99960216afb9860c7c647dbfd0c9
Ken Vu <ken@gladeye.co.nz>
2015-10-21 09:51:44 +1300

    Set to share all WP global variables to all views

--- src/Providers/LucentServiceProvider.php
+++ src/Providers/LucentServiceProvider.php
@@ -13,8 +13,28 @@ class LucentServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Register services
         $this->app->singleton('lucent.template', function($app) {
             return $app->make('Gladeye\Lucent\Template');
+        });
+    }
+
+    public function boot() {
+
+        add_action('wp', function() {
+            //Share all WP globals to all views
+            $vars = apply_filters('lucent_template_file_globals',
+                array('posts', 'post', 'wp_did_header', 'wp_did_template_redirect', 'wp_query', 'wp_rewrite', 'wpdb', 'wp_version', 'wp', 'id', 'comment', 'user_ID'));
+
+            if (is_array($vars)) {
+                $shares = [];
+                foreach ($vars as $global_var) {
+                    global $$global_var;
+                    $share[$global_var] = $$global_var;
+                }
+
+                view()->share($share);
+            }
         });
     }
 }
