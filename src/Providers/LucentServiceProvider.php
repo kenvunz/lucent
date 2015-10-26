e472c8c804a4d12066114f3f7356fe32a15d9d27
Ken Vu <ken@gladeye.co.nz>
2015-10-27 10:44:34 +1300

    Put all WP actions into `Wp/actions.php`

--- src/Providers/LucentServiceProvider.php
+++ src/Providers/LucentServiceProvider.php
@@ -37,20 +37,8 @@ class LucentServiceProvider extends ServiceProvider {
     public function boot() {
         $app = $this->app;
 
-        add_action('wp', function () use ($app) {
-            //Share all WP globals to all views
-            $vars = apply_filters('lucent_template_file_globals',
-                array('posts', 'post', 'wp_did_header', 'wp_did_template_redirect', 'wp_query', 'wp_rewrite', 'wpdb', 'wp_version', 'wp', 'id', 'comment', 'user_ID'));
-
-            if (is_array($vars)) {
-                $shares = [];
-                foreach ($vars as $global_var) {
-                    global $$global_var;
-                    $share[$global_var] = $$global_var;
-                }
-
-                view()->share($share);
-            }
+        $app->group(['namespace' => 'Gladeye\Lucent'], function ($app) {
+            require __DIR__ . '/../Wp/actions.php';
         });
 
         // even tho we don't use the `$view` but we have to `make` it here
@@ -60,10 +48,6 @@ class LucentServiceProvider extends ServiceProvider {
 
         // Extend Blade with custom Wordpress directives
         BladeExtension::attach($app, $blade);
-
-        $app->group(['namespace' => 'Gladeye\Lucent'], function ($app) {
-            require __DIR__ . '/../Wp/actions.php';
-        });
     }
 
     public function provides() {
--- src/Wp/actions.php
+++ src/Wp/actions.php
@@ -1,5 +1,26 @@
 <?php
 
+add_action('wp', function () use ($app) {
+    //Share all WP globals to all views
+    $vars = apply_filters('lucent_template_file_globals',
+        array('posts', 'post', 'wp_did_header', 'wp_did_template_redirect', 'wp_query',
+            'wp_rewrite', 'wpdb', 'wp_version', 'wp', 'id', 'comment', 'user_ID'));
+
+    if (is_array($vars)) {
+        $shares = [];
+        foreach ($vars as $global_var) {
+            global $$global_var;
+            $share[$global_var] = $$global_var;
+        }
+
+        view()->share($share);
+    }
+});
+
 add_action('admin_menu', function () {
-    remove_menu_page('themes.php');
+    $remove_items = apply_filters('lucent_remove_admin_menu_item', ['themes.php']);
+
+    foreach ($remove_items as $item) {
+        remove_menu_page($item);
+    }
 });
